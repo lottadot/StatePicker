cbe1fdc8cddd83caf58bb8fbb1dc0cb9ac1bcf9f
lottadot <shane@lottadot.com>
2013-02-04 19:57:37 -0500

    Add states data and loader to view controller

--- States/States/LDTViewController.m
+++ States/States/LDTViewController.m
@@ -9,7 +9,9 @@
 #import "LDTViewController.h"
 
 @interface LDTViewController ()
+@property (strong, nonatomic) NSArray *states;
 
+- (NSArray *)loadStatesWithError:(NSError *)error;
 @end
 
 @implementation LDTViewController
@@ -17,13 +19,27 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    NSError *statesLoadError = nil;
+    _states = [self loadStatesWithError:statesLoadError];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSArray *)loadStatesWithError:(NSError *)error
+{
+    NSString *statesDataFile = [[NSBundle mainBundle] pathForResource:@"states" ofType:@"json"];
+    NSData   *statesData = [NSData dataWithContentsOfFile:statesDataFile];
+    
+    id parsedJSON = [NSJSONSerialization JSONObjectWithData:statesData
+                                                    options:NSJSONReadingAllowFragments
+                                                      error:&error];
+    
+    NSArray *states = [parsedJSON valueForKeyPath:@"states.state"];
+    return states;
 }
 
 @end
